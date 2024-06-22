946d74d3781be74cfb4ff3e104dc6f8354b9cc7d
Erick Garcia <erickgegarcia@gmail.com>
2024-06-22 12:24:12 -0300

    start with createBrowserRouter and react-helmet-async

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+import './global.css'
+import { router } from './routes'
+
 export function App() {
-
   return (
-  <h1>Hello World</h1>
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | pizza.shop'/>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
-  
 }
-
